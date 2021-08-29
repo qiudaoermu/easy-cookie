5267e7974fa504e75415ce1b287a648f3976737a
chenzhenfei <793385821@qq.com>
2021-08-30 00:54:05 +0800

    [reflector]

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,33 @@
 // Project configuration.
-module.exports = function (grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
+const config = {
+  qunit: {
+    all: {
       options: {
-        stripBanners: true, //true 允许添加头部信息
-        banner: '/*! <%=pkg.name%>-<%=pkg.version%>.js <%=grunt.template.today("yyyy-mm-dd") %> */\n'//在头部添加js文件名和时间的注释
-      },
-      build: {
-        src: './index.js',
-        dest: 'build/js/<%=pkg.name%>-<%=pkg.version%>.min.js'
+        urls: [
+          'http://localhost:8000/test/test.html',
+        ]
       }
-    },
-    qunit: {
-      all: {
-        options: {
-          urls: [
-            'http://localhost:8000/test/test.html',
-          ]
-        }
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-          base: '.'
-        }
-      }
-    },
-    exec: {
-      rollup: 'npx rollup -c',
-      lint: 'npx standard',
-      format:
-        'npx prettier -l --write --single-quote --no-semi "**/*.{html,js,json,md,mjs,yml}" && npx eslint "**/*.{html,md}" --fix && npx standard --fix',
-      'browserstack-runner': 'node_modules/.bin/browserstack-runner --verbose'
     }
-  });
+  },
+  connect: {
+    server: {
+      options: {
+        port: 8000,
+        base: '.'
+      }
+    }
+  },
+  exec: {
+    rollup: 'npx rollup -c',
+    lint: 'npx standard',
+    format:
+      'npx prettier -l --write --single-quote --no-semi "**/*.{html,js,json,md,mjs,yml}" && npx eslint "**/*.{html,md}" --fix && npx standard --fix',
+    'browserstack-runner': 'node_modules/.bin/browserstack-runner --verbose'
+  }
+}
+module.exports = function (grunt) {
+  grunt.initConfig(config);
 
-  // This plugin provides the "connect" task.
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-contrib-connect');
